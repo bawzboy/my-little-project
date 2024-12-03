00574b62e2d83f7d40907be3a4e21b4c4a7f4e88
Helge <helgepeggen@web.de>
2024-12-03 12:15:09 +0100

    .

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -3,9 +3,10 @@ import Link from 'next/link';
 export default function Navigation() {
     return (
         <div>
-            <nav className="container relative flex space-x-4 items-center p-4">
+            <nav className="container flex space-x-4 items-center p-4">
                 <Link className="text-2xl" href="/">Home</Link>
-                <div>
+                <div className="container flex space-x-4 items-center">
+                    <Link href="/archive">Archive</Link>
                     <Link href="/about">About</Link>
                 </div>
             </nav>
